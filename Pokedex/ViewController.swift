a26c0bb8d0c142b59c8630bdbbd1992b731a6e57
Yusuf Koroglu <yusufkerimkoroglu@gmail.com>
2022-04-13 17:37:07 -0500

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Yusuf Koroglu on 4/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
